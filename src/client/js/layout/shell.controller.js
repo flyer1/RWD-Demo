b10893011aaa5f44878563f5627a7d9290d067e2
Jon Kroeker <jon.kroeker2@gmail.com>
2015-04-08 11:36:10 -0500

    Responsive treatment to the main nabber.

--- src/client/js/layout/shell.controller.js
+++ src/client/js/layout/shell.controller.js
@@ -1,44 +1,43 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  angular
-    .module('app')
-    .controller('ShellController', ShellController);
+    angular
+        .module('app')
+        .controller('ShellController', ShellController);
 
-  ShellController.$inject = ['$state', 'datacontext'];
+    ShellController.$inject = ['$state', 'datacontext'];
 
-  function ShellController($state, datacontext) {
-    var vm = this;
-    var siteRepo = datacontext.getSiteRepository();
-    vm.navItems = siteRepo.navItems;
-    vm.currNavItem = vm.navItems[1];
-    vm.currSubNavItem = vm.currNavItem.children[0];
-    vm.isActiveNavItem = isActiveNavItem;
-    vm.isActiveSubNavItem = isActiveSubNavItem;
+    function ShellController($state, datacontext) {
+        var vm = this;
+        var siteRepo = datacontext.getSiteRepository();
+        vm.navItems = siteRepo.navItems;
+        vm.currNavItem = vm.navItems[1];
+        vm.currSubNavItem = vm.currNavItem.children[0];
+        vm.isActiveNavItem = isActiveNavItem;
+        vm.isActiveSubNavItem = isActiveSubNavItem;
+        vm.setActiveNavItem = setActiveNavItem;
+        vm.setActiveSubNavItem = setActiveSubNavItem;
+        console.log(vm.currNavItem);
+        return;
 
-    vm.setActiveNavItem = setActiveNavItem;
-    vm.setActiveSubNavItem = setActiveSubNavItem;
-    console.log(vm.currNavItem);
-    return;
+        /////////// IMPLEMENTATION /////////////////
 
-    /////////// IMPLEMENTATION /////////////////
+        function setActiveNavItem(navItem) {
+            vm.currNavItem = navItem;
+            //console.log('active nav is ' + vm.currNavItem.code);
+        }
 
-    function setActiveNavItem(navItem) {
-      vm.currNavItem = navItem;
-      //console.log('active nav is ' + vm.currNavItem.code);
+        function isActiveNavItem(navItem) {
+            return vm.currNavItem.code === navItem.code;
+        }
+
+        function setActiveSubNavItem(subNavItem) {
+            vm.currSubNavItem = subNavItem;
+            //console.log('active sub nav is ' + vm.currSubNavItem.code);
+        }
+
+        function isActiveSubNavItem(subNavItem) {
+            return vm.currSubNavItem.code === subNavItem.code;
+        }
     }
-
-    function isActiveNavItem(navItem) {
-      return vm.currNavItem.code === navItem.code;
-    }
-
-    function setActiveSubNavItem(subNavItem) {
-      vm.currSubNavItem = subNavItem;
-      //console.log('active sub nav is ' + vm.currSubNavItem.code);
-    }
-
-    function isActiveSubNavItem(subNavItem) {
-      return vm.currSubNavItem.code === subNavItem.code;
-    }
-  }
 })();
